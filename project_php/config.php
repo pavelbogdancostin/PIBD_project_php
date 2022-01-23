79481b52a363e09f160fdea17e93aea205d8f63b
Pavel Bogdan <78963505+pavelbogdancostin@users.noreply.github.com>
2022-01-23 18:28:28 +0200

    Update config.php

--- project_php/config.php
+++ project_php/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', '127.0.0.1');
 define('DB_NAME', 'tema_pibd');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'omen123');
+define('DB_PASSWORD', '*******');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
